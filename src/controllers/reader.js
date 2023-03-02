17a9f7a63ae4728c0569f5e002a7b7d4dfb9e713
Sarah Moran <s.l.a.moran@gmail.com>
2023-03-02 19:22:41 +0000

    completion of readersById controller and test, prettier applied

--- src/controllers/reader.js
+++ src/controllers/reader.js
@@ -1,13 +1,35 @@
 const { Reader } = require("../models");
 
 const create = async (req, res) => {
-  const newReader = await Reader.create(req.body);
-  res.status(200).json(newReader);
+  try {
+    const newReader = await Reader.create(req.body);
+    res.status(200).json(newReader);
+  } catch (e) {
+    res.status(500).json(e.message);
+  }
 };
 
 const readers = async (req, res) => {
-  const allReaders = await Reader.findAll();
-  res.status(200).json(allReaders);
+  try {
+    const allReaders = await Reader.findAll();
+    res.status(200).json(allReaders);
+  } catch (e) {
+    res.status(500).json(e.message);
+  }
 };
 
-module.exports = { create, readers };
+const readersById = async (req, res) => {
+  try {
+    const readerId = req.params.id;
+    const reader = await Reader.findByPk(readerId);
+
+    if (!reader) {
+      res.status(404).json({ error: "The reader could not be found." });
+    }
+    res.status(200).json(reader);
+  } catch (e) {
+    res.status(500).json(e.message);
+  }
+};
+
+module.exports = { create, readers, readersById };
--- src/routes/reader.js
+++ src/routes/reader.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const readerController = require("../controllers/reader");
+const reader = require("../models/reader");
 
 const router = express.Router();
 
 router.post("/", readerController.create);
 
 router.get("/", readerController.readers);
+
+router.get("/:id", readerController.readersById);
 
 module.exports = router;
