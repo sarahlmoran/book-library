96927bfa1f44899866dd7d3add307bf863b267bb
Sarah Moran <s.l.a.moran@gmail.com>
2023-03-02 18:50:03 +0000

    completion of find all readers controller and function with prettier applied

--- src/controllers/reader.js
+++ src/controllers/reader.js
@@ -2,7 +2,12 @@ const { Reader } = require("../models");
 
 const create = async (req, res) => {
   const newReader = await Reader.create(req.body);
-  res.status(201).json(newReader);
+  res.status(200).json(newReader);
 };
 
-module.exports = { create };
+const readers = async (req, res) => {
+  const allReaders = await Reader.findAll();
+  res.status(200).json(allReaders);
+};
+
+module.exports = { create, readers };
--- src/routes/reader.js
+++ src/routes/reader.js
@@ -5,4 +5,6 @@ const router = express.Router();
 
 router.post("/", readerController.create);
 
+router.get("/", readerController.readers);
+
 module.exports = router;
